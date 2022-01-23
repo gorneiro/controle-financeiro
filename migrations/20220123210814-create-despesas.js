df62a846db71d98706b6667f13dab2756d0da260
Mateus Carneiro <mateus-junior@hotmail.com>
2022-01-23 18:32:43 -0300

    Criando as tabelas despesas e receitas

---
+++ migrations/20220123210814-create-despesas.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('despesas', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      descricao: {
+        type: Sequelize.STRING
+      },
+      valor: {
+        type: Sequelize.BIGINT
+      },
+      data: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('despesas');
+  }
+};
